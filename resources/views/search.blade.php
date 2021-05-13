40a862b81ca9e51b4d427c5e0cd9d45acaa3041a
Wren Parker <Wrenparker@gmail.com>
2021-05-13 09:17:35 -0400

    mobilestuff

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -9,16 +9,63 @@
     </div>
     <div class="container">
       <h1>Results</h1>
-        <ul>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-          <li>item</li>
-        </ul>
+        <div class="row" id="Header">
+          <div class="col-md-3">
+            File Name
+          </div>
+          <div class="col-md-3" id="hide-on-mobile">
+            File Type
+          </div>
+          <div class="col-md-3" id="hide-on-mobile">
+            File Size
+          </div>
+          <div class="col-md-3" id="hide-on-mobile">
+            Date Modified
+          </div>
+        </div>
+        <hr/>
+        <div class="row py-2" id="result">
+          <div class="col-md-3">
+            Training Manual
+          </div>
+          <div class="col-md-3" id="hide-on-mobile">
+            pdf
+          </div>
+          <div class="col-md-3" id="hide-on-mobile">
+            20M
+          </div>
+          <div class="col-md-3" id="hide-on-mobile">
+            5/13/2021
+          </div>
+        </div>
+        <div class="row py-2" id="result">
+          <div class="col-md-3">
+            First Responder Video
+          </div>
+          <div class="col-md-3" id="hide-on-mobile">
+            mp4
+          </div>
+          <div class="col-md-3" id="hide-on-mobile">
+            500M
+          </div>
+          <div class="col-md-3" id="hide-on-mobile">
+            5/13/2021
+          </div>
+        </div>
+        <div class="row py-2" id="result">
+          <div class="col-md-3">
+            Textbook
+          </div>
+          <div class="col-md-3" id="hide-on-mobile">
+            pdf
+          </div>
+          <div class="col-md-3" id="hide-on-mobile">
+            332M
+          </div>
+          <div class="col-md-3" id="hide-on-mobile">
+            5/13/2021
+          </div>
+        </div>
     </div>
     <div style="background-color: white">
       @include('includes.footer')
